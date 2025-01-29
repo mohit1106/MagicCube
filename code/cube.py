38bece83438f9191bf7e74645775da6a455a4916
mohit1106 <118046944+mohit1106@users.noreply.github.com>
2025-01-29 12:55:05 +0530

    cube.py

--- code/cube.py
+++ code/cube.py
@@ -1,14 +1,4 @@
-"""
-This file is part of the Magic Cube project.
 
-license
--------
-Copyright 2012 David W. Hogg (NYU).
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or (at
-your option) any later version.
 
 This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
